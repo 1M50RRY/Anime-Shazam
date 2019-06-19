1b7002b6d5ef65c562b44ae502e73fe44b6c89e9
ims0rry <38806471+1M50RRY@users.noreply.github.com>
2019-06-20 00:23:58 +0300

    Created mockup

--- App.js
+++ App.js
@@ -1,29 +1,46 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { StyleSheet, View, Image, Dimensions } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+import { Button, Icon, Text } from 'native-base';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+const {width, height} = Dimensions.get('window');
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const options = {noData: true};
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  state = { photo: null };
+
+  handleChoosePhoto = () => {
+    ImagePicker.launchImageLibrary(options, response => {
+      console.log(this.state);
+      if (response.uri) this.setState({photo: response});
+    });
+  }
+
+  handleTakePhoto = () => {
+    ImagePicker.launchCamera(options, response => {
+      if (response.uri) this.setState({photo: response});
+    });
+  }
+
   render() {
+    const { photo } = this.state;
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        {photo && (
+          <Image 
+            source={{uri: photo.uri}} 
+            style={{width: 300, height: 300}}
+          />
+        )}
+        <Button style={{marginTop: height - 100}} onPress={this.handleTakePhoto} iconLeft rounded primary>
+          <Icon name='camera' />
+          <Text>Take a photo</Text>
+        </Button>
+        <Button style={{marginTop: height - 100, marginLeft: 10}} onPress={this.handleChoosePhoto} rounded iconLeft primary>
+          <Icon name='md-images' />
+          <Text>Pick from gallery</Text>
+        </Button>
       </View>
     );
   }
@@ -35,15 +52,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    flexDirection:'row', 
+    flexWrap:'wrap'
+  }
 });
